420f5f6d62cfbd11360d997136d4a7edb6f6f3ce
mariannacoppa <densimetra@gmail.com>
2024-09-10 18:21:04 +0200

    [UPD] made axios call

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 const { createApp } = Vue;
 
 createApp({
-
+    data() {
+        return {
+            apiUrl: 'server.php'
+        }
+    },
+    methods: {
+        getToDoList() {
+            axios.get(this.apiUrl).then((result) => {
+                console.log(result.data);
+            });
+        }
+    },
 }).mount('#app');
