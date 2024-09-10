4d2751720059bab83fc7493d798505ad5554ffa6
mariannacoppa <densimetra@gmail.com>
2024-09-10 18:36:22 +0200

    [UPD] printed string in console FINE ESERCIZIO BASE

--- server.php
+++ server.php
@@ -2,4 +2,7 @@
     $string = file_get_contents('./data/todo-list.json');
     $todolist = json_decode($string);
     var_dump($todolist);
+
+    header('Content: application/json');
+    echo $string;
 ?>
